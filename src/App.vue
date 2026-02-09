3723acc38bb99e6720f6dea72278603445d64c0c
Kaeden Bryer <130073365+kaeden-bryer@users.noreply.github.com>
2026-02-09 15:50:20 -0500

    refactor directory

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <h1>Rock Paper Scissors</h1>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
